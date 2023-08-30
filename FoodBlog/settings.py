39d0d17d53dd07473e667eb70ff6dc438f26ff10
Calvin G <calcal56@gmail.com>
2023-08-30 18:56:47 -0500

    Changing from MySql to sqlite
    Doing this to see if Heroku will show mercy lol.

--- FoodBlog/settings.py
+++ FoodBlog/settings.py
@@ -79,20 +79,27 @@ WSGI_APPLICATION = 'FoodBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': NAME,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASSWORD,
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': NAME,
+#        'USER': DB_USER,
+#        'PASSWORD': DB_PASSWORD,
+#        'HOST': '127.0.0.1',
+#        'PORT': '3306',
+#        'OPTIONS': {
+#            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#        }
+#
+#    }
+#}
 
+DATABASES = { 
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        } 
     }
-}
 
 
 # Password validation
