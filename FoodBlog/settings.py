c0b3ae0e396fb63d9348fd271c8e92854cdf9efa
Calvin G <calcal56@gmail.com>
2023-08-31 14:02:47 -0500

    our slug was too large.
    removed whitenoise middleware.

--- FoodBlog/settings.py
+++ FoodBlog/settings.py
@@ -78,7 +78,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'FoodBlog.urls'
