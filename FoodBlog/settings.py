9e896ec01b058b067015ded3db8b1d0546605059
Calvin G. <calcal56@gmail.com>
2023-09-07 12:58:18 -0500

    connecting to heroku db

--- FoodBlog/settings.py
+++ FoodBlog/settings.py
@@ -117,12 +117,16 @@ WSGI_APPLICATION = 'FoodBlog.wsgi.application'
 
 
 
-DATABASES = { 
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        } 
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('HEROKU_DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': os.environ.get('HOST'),
+        'PORT':  os.environ.get('PORT')
     }
+}
 
 
 # Password validation
