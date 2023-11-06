6ea1164cd007c9e6f388f207053cb9d369360619
Calvin G. <calcal56@gmail.com>
2023-11-06 10:02:08 -0600

    Added the receivers back. Basically I got locked out because a defualt user profile picture didn't exist and when I tried to create an account it would crash. So I took out that code temporarily and now am adding it back after uploading the image

--- recipes/models.py
+++ recipes/models.py
@@ -230,7 +230,7 @@ class UserProfile(models.Model):
 
 
 
-  #  @receiver(post_save, sender=User)
+    @receiver(post_save, sender=User)
 
     def create_user_profile(sender, instance, created, **kwargs):
 
@@ -248,7 +248,7 @@ class UserProfile(models.Model):
 
 
 
- #   @receiver(post_save, sender=User)
+    @receiver(post_save, sender=User)
 
     def save_user_profile(sender, instance, **kwargs):
 
