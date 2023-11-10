2fcd81c01bec06c4433f5c1ae918a5feba81fe04
Calvin G. <calcal56@gmail.com>
2023-11-09 21:57:03 -0600

    got the login page lookin niiiice

--- FoodBlog/urls.py
+++ FoodBlog/urls.py
@@ -17,6 +17,12 @@ from django.conf.urls.static import static
 from django.contrib.auth.forms import AuthenticationForm
 
 
+from django.urls import path
+
+from django.contrib.auth import views as auth_views
+
+
+
 print("URLS . PY TRIGGERES")
 
 urlpatterns = [
@@ -27,6 +33,15 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path("base/", include("recipes.urls")),
     path("accounts/", include("recipes.urls")),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 
     
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
